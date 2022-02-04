e07a63709b64347774d4c2ed02b69f79755d63c5
Joseph Lee <jlee190@hotmail.com>
2022-02-04 00:58:15 -0500

    story

---
+++ index.js
@@ -0,0 +1,10 @@
+let 'firstchoice' = window.prompt(`which do you choose charmander, bulbasaur, or squirtle?`)
+    if (firstchoice === 'charmander') {
+        let secondchoice = window.prompt(`learn new move 'ember' or 'scratch'.`)
+            if (secondchoice === 'ember') {
+                let thirdchoice = window.prompt(`charmander is evolving. let him evolve to charmeleon? 'yes' or 'no'?`)
+                if (thirdchoice === yes) {
+                    let fourthchoice = window.prompt(`charmander has evolved to charmeleon.`)
+                }
+            }
+    }
